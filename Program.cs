286e17169ff884c3ec8bc95c335e29e4b56239dd
Robbert de Groot <zekaric@gmail.com>
2021-02-15 12:55:05 -0800

    Initial commit of the simple sync program.

---
+++ Program.cs
@@ -0,0 +1,369 @@
+/******************************************************************************
+file:       ZSync
+author:     Robbert de Groot
+copyright:  2012, Robbert de Groot
+
+description:
+Synchronize two folders.
+******************************************************************************/
+
+/******************************************************************************
+MIT License
+
+Copyright (c) 2012, Robbert de Groot
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+******************************************************************************/
+
+// Using //////////////////////////////////////////////////////////////////////
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace zsync
+{
+    class Program
+    {
+        private const int indexSRC = 0;
+        private const int indexDST = 1;
+
+        // Main /////////////////////////////////////////////////////////////////
+        static int Main(string[] args)
+        {
+            int             index;
+            string          spath,
+                            dpath,
+                            exclude;
+            List<string>    spathList,
+                            dpathList,
+                            excludeExt;
+            List<FileInfo>  sList,
+                            dList;
+
+            // Print the header.
+            Console.Write(
+               "==============================================================================\n" +
+               "zsync\n" +
+               "==============================================================================\n\n");
+
+            // Not enough arguments then inform the user on how to use the program.
+            if (args.Length < 2)
+            {
+                Console.Write(
+                   "zsync [sourcePath] [destinationPath] [-e[.ext]...\n\n" +
+                   "[sourcePath]        - Path of the source.  Include drive. Eg: D:\\, C:\\temp\\\n" +
+                   "[destinationPath]   - Path of the destination.\n" +
+                   "[e[.ext]...         - List the file extensions to filter out.\n\n");
+                return 0;
+            }
+
+            // Get the source and destination paths.
+            spath = args[indexSRC];
+            dpath = args[indexDST];
+            exclude = "";
+            if (args.Length >= 3)
+            {
+                exclude = args[2];
+            }
+
+            // Make sure there is a terminating \
+            if (spath.Substring(spath.Length - 1).CompareTo("\\") != 0)
+            {
+                spath += "\\";
+            }
+            if (dpath.Substring(dpath.Length - 1).CompareTo("\\") != 0)
+            {
+                dpath += "\\";
+            }
+
+            // Check if the directories exist.
+            if (!Directory.Exists(spath))
+            {
+                Console.Write(
+                   "ERROR: Source directory doesn't exist.\n" +
+                   args[indexSRC] +
+                   "\n");
+                return 1;
+            }
+
+            if (!Directory.Exists(dpath))
+            {
+                Console.Write(
+                   "ERROR: Destination direction doesn't exist.\n" +
+                   args[indexDST] +
+                   "\n");
+                return 2;
+            }
+
+            // Get the excludes
+            excludeExt = null;
+            if (exclude.Length > 0)
+            {
+                excludeExt = exclude.Split('.').ToList<String>();
+                excludeExt.RemoveAt(0);
+
+                // Put the . back.
+                for (index = 0; index < excludeExt.Count; index++)
+                {
+                    excludeExt[index] = "\\." + excludeExt[index] + "$";
+                }
+            }
+
+            // Fetch all the files found in the two directories.
+            Console.Write("- Reading source tree.      ");
+            spathList = _GetFiles(spath, excludeExt);
+            Console.Write("File Count: " + spathList.Count() + "\n");
+            Console.Write("- Reading destination tree. ");
+            dpathList = _GetFiles(dpath, null);
+            Console.Write("File Count: " + dpathList.Count() + "\n");
+
+            // Sort them and get the file information for each file.
+            Console.Write("- Prepping trees.\n");
+            sList = _PrepDir(spath, spathList);
+            dList = _PrepDir(dpath, dpathList);
+
+            // Do the synchronization/mirroring.
+            Console.Write("- Synchonizing.\n");
+            _Sync(spath, spathList, sList, dpath, dpathList, dList);
+
+            Console.Write("- Finished.\n");
+            return 0;
+        }
+
+        // _GetFiles ////////////////////////////////////////////////////////////
+        // Recursively fetches all the files in the directory.
+        static private List<string> _GetFiles(string path, List<String> excludeExt)
+        {
+            int index,
+                         eindex,
+                         findex;
+            string[] fileList,
+                         dirList;
+            List<string> pathList,
+                         pathListTemp;
+
+            // Ignore recycling bin.
+            if (path.IndexOf("$RECYCLE") >= 0)
+            {
+                return null;
+            }
+
+            // Get the directories.
+            dirList = Directory.GetDirectories(path, "*.*", SearchOption.TopDirectoryOnly);
+            // Get the files in this directory.
+            fileList = Directory.GetFiles(path, "*.*", SearchOption.TopDirectoryOnly);
+
+            pathList = new List<string>();
+
+            // Append the local files to the path list.
+            for (findex = 0; findex < fileList.Length; findex++)
+            {
+                pathList.Add(fileList[findex]);
+            }
+
+            // For all directories.
+            for (index = 0; index < dirList.Length; index++)
+            {
+                try
+                {
+                    // Get their files.
+                    pathListTemp = _GetFiles(dirList[index], excludeExt);
+
+                    // Append their files to the path list.
+                    for (findex = pathListTemp.Count() - 1; findex >= 0; findex--)
+                    {
+                        if (excludeExt != null)
+                        {
+                            for (eindex = 0; eindex < excludeExt.Count; eindex++)
+                            {
+                                if (Regex.Match(pathListTemp[findex], excludeExt[eindex]).Success)
+                                {
+                                    break;
+                                }
+                            }
+
+                            // We found a match so don't include this file.
+                            if (eindex != excludeExt.Count)
+                            {
+                                continue;
+                            }
+                        }
+
+                        pathList.Add(pathListTemp[findex]);
+                    }
+                }
+                catch
+                {
+                }
+            }
+
+            return pathList;
+        }
+
+        // _MakeDir /////////////////////////////////////////////////////////////
+        // Create a directory if it is missing.
+        static bool _MakeDir(string dir)
+        {
+            // Pop the filename
+            string dirPopped;
+            int index;
+
+            // Oh swell, the directory exists.
+            if (Directory.Exists(dir))
+            {
+                return true;
+            }
+
+            // Pop off a directory and check again.
+            index = dir.LastIndexOf('\\');
+            dirPopped = dir.Substring(0, index);
+            _MakeDir(dirPopped);
+
+            // Create the directory.
+            Directory.CreateDirectory(dir);
+
+            return true;
+        }
+
+        // _PrepDir /////////////////////////////////////////////////////////////
+        // Sort the path list and fetch the file information for each file.
+        static private List<FileInfo> _PrepDir(string path, List<string> pathList)
+        {
+            int index;
+            FileInfo fileInfo;
+            List<FileInfo> fileList = new List<FileInfo>();
+
+            pathList.Sort();
+            for (index = 0; index < pathList.Count(); index++)
+            {
+                fileInfo = new FileInfo(pathList[index]);
+
+                fileList.Add(fileInfo);
+                pathList[index] = pathList[index].Substring(path.Length);
+            }
+
+            return fileList;
+        }
+
+        // _Sync ////////////////////////////////////////////////////////////////
+        // Synch the two directories.
+        static private bool _Sync(string spath, List<string> spathList, List<FileInfo> sList, string dpath, List<string> dpathList, List<FileInfo> dList)
+        {
+            int sindex,
+                dindex;
+
+            sindex    =
+               dindex = 0;
+            for (;;)
+            {
+                // We are done when there is nothing left to check.
+                if (sindex == spathList.Count() &&
+                    dindex == dpathList.Count())
+                {
+                    break;
+                }
+
+                // Files found with the same name.
+                // Only valid if there is anything left in the destination list to check.
+                if      (dindex                                         <  dpathList.Count()  &&
+                         sindex                                         != spathList.Count()  &&
+                         dpathList[dindex].CompareTo(spathList[sindex]) == 0)
+                {
+                    // For some reason sList[sindex] may not have valid data.  It will except.
+                    // So this try block is here to catch that.  Maybe something to do with permissions.
+                    try
+                    {
+                        // Why not just dList[dindex].LastWriteTimeUtc < sList[sindex].LastWriteTimeUtc?
+                        // The millisecond tick count on my QNAP will be off by a few even for something that was 
+                        // just copied over.  So I just check down to the 'second' level and no more.
+                        if (dList[dindex].LastWriteTimeUtc.Year < sList[sindex].LastWriteTimeUtc.Year                                           ||
+                            (dList[dindex].LastWriteTimeUtc.Year == sList[sindex].LastWriteTimeUtc.Year                                     &&
+                             (dList[dindex].LastWriteTimeUtc.DayOfYear < sList[sindex].LastWriteTimeUtc.DayOfYear                       ||
+                              (dList[dindex].LastWriteTimeUtc.DayOfYear == sList[sindex].LastWriteTimeUtc.DayOfYear                 &&
+                               (dList[dindex].LastWriteTimeUtc.Hour < sList[sindex].LastWriteTimeUtc.Hour                       ||
+                                (dList[dindex].LastWriteTimeUtc.Hour == sList[sindex].LastWriteTimeUtc.Hour                 &&
+                                 (dList[dindex].LastWriteTimeUtc.Minute < sList[sindex].LastWriteTimeUtc.Minute         ||
+                                  (dList[dindex].LastWriteTimeUtc.Minute == sList[sindex].LastWriteTimeUtc.Minute   &&
+                                   dList[dindex].LastWriteTimeUtc.Second < sList[sindex].LastWriteTimeUtc.Second))))))))
+                        {
+                            // Try to copy the file.
+                            try
+                            {
+                                Console.Write("> " + spathList[sindex] + "\n");
+                                File.Copy(spath + spathList[sindex], dpath + dpathList[dindex], true);
+                            }
+                            catch
+                            {
+                                Console.Write("   Copy Failed.\n");
+                            }
+                        }
+                    }
+                    catch
+                    {
+                        Console.Write(
+                           "ERROR: Received an exception on valid data.  Ignoring.\n" +
+                           " file: " + spathList[sindex] + "\n");
+                    }
+                    dindex++;
+                    sindex++;
+                }
+                // Destination file is no longer around.
+                else if (dindex < dpathList.Count() &&
+                         (sindex == spathList.Count() ||
+                          dpathList[dindex].CompareTo(spathList[sindex]) < 0))
+                {
+                    // Try to delete the file.
+                    try
+                    {
+                        Console.Write("X " + dpathList[dindex] + "\n");
+                        File.Delete(dpath + dpathList[dindex]);
+                    }
+                    catch
+                    {
+                        Console.Write("   Delete failed.\n");
+                    }
+                    dindex++;
+                }
+                // Source file is not on the destination.
+                else
+                {
+                    // try and copy the new file.
+                    try
+                    {
+                        Console.Write("N " + spathList[sindex] + "\n");
+
+                        // Make sure the directory is created first.
+                        _MakeDir(Path.GetDirectoryName(dpath + spathList[sindex]));
+
+                        File.Copy(spath + spathList[sindex], dpath + spathList[sindex]);
+                    }
+                    catch
+                    {
+                        Console.Write("   Copy failed.\n");
+                    }
+                    sindex++;
+                }
+            }
+
+            return true;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-# ZSync
-Zekaric Sync.  Simple directory synchronize program.  
+
+# Zekaric : Sync
+
+
+**`Author:   `** Robbert de Groot
+
+**`Copyright:`** 2012, Robbert de Groot
+
+**`License:  `** MIT
+
+## Description:
+
+
+A simply directory synchronization program.  It will make a directory exactly match another directory.
+
+## Use:
+
+
+zsync [sourcePath] [destinationPath] [-e[.ext]+]
+
+**[sourcePath]**<br>
+    Path of the source directory.  Include drive letter. Eg: D:\\, C:\\temp\\
+
+**[destinationPath]**<br>
+    Path of the destination.  Include drive letter.
+
+**[e[.ext]+]**<br>
+    List the file extensions to filter out.  No spaces between extensions.  Extensions include the '.'.
+
+
+## Note:
+
+
+The reason I wrote this tool is because I just needed something simple to synchronize my working directories on the hard drive of may main machine to a directory on a Network Attached Storage unit (NAS).  I found a lot of tools that would do this out there but a lot have become overly complicated and not what I am looking for.
